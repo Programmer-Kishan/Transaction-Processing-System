7d6f2dccbfcf1a4f47442ab7f9377accef600755
kishan kathare <kishankathare45@gmail.com>
2024-07-21 23:39:30 +0530

    made charts responsive

--- src/Components/Insights/Charts.tsx
+++ src/Components/Insights/Charts.tsx
@@ -7,15 +7,15 @@ const Charts = () => {
     return (
         <div className="w-full max-h-fit bg-grayish-white dark:bg-dark-gray flex flex-col justify-center items-center overflow-y-scroll">
             <div className="w-full text-center">
-                <h1 className="w-full font-montserrat font-bold text-3xl my-5">Types - count of each type</h1>
+                <h1 className="w-full font-montserrat font-bold text-3xl my-5 text-black dark:text-grayish-white">Types - count of each type</h1>
                 <BarChartType />
             </div>
             <div className="w-full text-center">
-                <h1 className="w-full font-montserrat font-bold text-3xl my-5">Currency - total amount of each currency</h1>
+                <h1 className="w-full font-montserrat font-bold text-3xl my-5 text-black dark:text-grayish-white">Currency - total amount of each currency</h1>
                 <BarChartCurrency />
             </div>
             <div className="w-full text-center">
-                <h1 className="w-full font-montserrat font-bold text-3xl my-5">Category - count of each category</h1>
+                <h1 className="w-full font-montserrat font-bold text-3xl my-5 text-black dark:text-grayish-white">Category - count of each category</h1>
                 <BarChartCategory />
             </div>
         </div>
